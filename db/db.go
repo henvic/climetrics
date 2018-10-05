a7fe6f9f513bf958f24744478219f2e618403c02
Henrique Vicente <henriquevicente@gmail.com>
2018-10-06 01:21:17 +0200

    db: adding package.

---
+++ db/db.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/hashicorp/errwrap"
+	"github.com/jmoiron/sqlx"
+)
+
+var db *sqlx.DB
+
+// Conn is a handle for the database
+func Conn() *sqlx.DB {
+	return db
+}
+
+// Load DB configuration and ping the server.
+func Load(ctx context.Context, dsn string) (err error) {
+	db, err = sqlx.Open("postgres", dsn)
+
+	if err != nil {
+		return errwrap.Wrapf("can't open connection to database: {{err}}", err)
+	}
+
+	if err := db.PingContext(ctx); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "can't ping database: %v\n", err)
+	}
+
+	return nil
+}
