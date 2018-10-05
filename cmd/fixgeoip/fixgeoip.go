b63c0d5309b6d9a742c3efcb613bbab9053045f1
Henrique Vicente <henriquevicente@gmail.com>
2018-10-06 01:23:05 +0200

    cmd: adding command to fix geolocation data on metrics entries.

---
+++ cmd/fixgeoip/fixgeoip.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/henvic/climetrics/db"
+	"github.com/henvic/climetrics/metrics"
+	_ "github.com/lib/pq"
+)
+
+var dsn string
+
+func setup(ctx context.Context) error {
+	return db.Load(ctx, dsn)
+}
+
+func run() error {
+	rand.Seed(time.Now().UTC().UnixNano())
+	flag.Parse()
+
+	ctx := context.Background()
+
+	if err := setup(ctx); err != nil {
+		return err
+	}
+
+	missing, err := metrics.MissingGeolocation(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	return add(ctx, missing)
+}
+
+func add(ctx context.Context, ips []string) error {
+	var errored []string
+
+	for _, ip := range ips {
+		updated, err := metrics.AddGeolocationIP(ctx, ip)
+
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "cannot find geolocation for IP %s: %v\n", ip, err)
+			errored = append(errored, ip)
+			continue
+		}
+
+		fmt.Printf("%d entries updated with IP %s\n", updated, ip)
+	}
+
+	if len(errored) != 0 {
+		return fmt.Errorf("failed to gather geolocation information for %d IPs", len(errored))
+	}
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%+v\n", err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	flag.StringVar(&dsn, "dsn", "postgres://admin@/climetrics?sslmode=disable", "dsn (PostgreSQL)")
+}
